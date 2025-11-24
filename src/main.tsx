cf54434b505c47ebe399410395e583610486779d
gansik92 <dzmitry.kavunou@icloud.com>
2025-11-24 15:54:03 +0100

    Replace App component with DashboardPage

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,9 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import DashboardPage from './pages/DashboardPage';
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <DashboardPage />
   </React.StrictMode>
 );
