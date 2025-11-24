31dd1a66725d20ca97bb039a01c8b0bf394182df
gansik92 <dzmitry.kavunou@icloud.com>
2025-11-24 22:12:37 +0000

    cleanup before filter-branch

--- src/main.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import DashboardPage from './pages/DashboardPage';
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <DashboardPage />
-  </React.StrictMode>
-);
